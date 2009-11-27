6d4b34a9dbf263dc6db02a0f5ad373b1d26a6cde
Patryk Połomski <cykuss@gmail.com>
2009-11-27 22:00:18 +0100

    kolejne poprawki GoogleApps

--- src/libmoor/Mailboxes.h
+++ src/libmoor/Mailboxes.h
@@ -45,7 +45,17 @@ enum MBoxID {
 	PocztaOnet= -86,
 	OnetEu    = 71,
 	GmxCom    = 70,
-	RockCom   = 23
+        RockCom   = 23,
+        MaiboxHu  = 87,
+        HotmailCom= -28,
+        InterfreeIt = -124,
+        TiscaliIt = -100,
+        VivapolskaTv= -11,
+        WippiesCom= 125,
+        GdeRu= -9,
+        KlikniCz= -65,
+        LivedoorCom= -30,
+        OneindiaIn= 55
 };
 
 std::string getMailboxName(int id);
