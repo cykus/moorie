222d07ac034e56d1f454a1a6a2a0535155aa0a18
swiezy <swiezy@zoho.com>
2010-02-06 23:54:45 +0100

    Fix adresse

--- src/libmoor/LibMoor.cpp
+++ src/libmoor/LibMoor.cpp
@@ -164,7 +164,8 @@ int CLibMoor::startDownload() {
 	return segValid;
 }
 
-int CLibMoor::selectUploadMailBox(std::string login, std::string passwd, std::string adressee) {
+
+int CLibMoor::selectUploadMailBox(std::string login, std::string passwd, std::string adressee, std::string downPasswd, std::string editPasswd) {
 
 // 	myUploadMailbox = getMailboxName(id);
         boost::regex mail_rgx("^(.*)@(.*)");
@@ -172,7 +173,9 @@ int CLibMoor::selectUploadMailBox(std::string login, std::string passwd, std::st
         boost::regex_match(login, result_sth, mail_rgx);
 
         myLogin = result_sth[1];
-	myPasswd = passwd;
+        myPasswd = passwd;
+        myDownPasswd = downPasswd;
+        myEditPasswd = editPasswd;
 	int adr_size = adressee.size();
 	int i2=0;
 	for (int i = 0; i<adr_size; ++i)
@@ -182,10 +185,10 @@ int CLibMoor::selectUploadMailBox(std::string login, std::string passwd, std::st
 			address.push_back(adressee.substr(i2, i));
 			i2=i+1;
 		}
-		else if(i == (adr_size))
-			address.push_back(adressee.substr(i2,i));
+		else if(i == (adr_size-1))
+			address.push_back(adressee.substr(i2,i+1));
 	}
-    myUploadMailbox = result_sth[2]; // TODO - wybieranie skrzynki po id;
+        myUploadMailbox = result_sth[2]; // TODO - wybieranie skrzynki po id;
 	return 0;
 }
 
@@ -233,10 +236,9 @@ int CLibMoor::splitFile(std::string filename, int size) {
 int CLibMoor::startUpload(unsigned int fromseg) {
 	LOG(Log::Info, boost::format("Zaczynam upload od segmentu: %1%") %fromseg);
 
-	std::stringstream ss;
+        std::stringstream ss;
 	std::string upload_mailbox = myLogin+"@"+myUploadMailbox;
-
-	myMailBox = MailboxFactory::Instance().Create(myUploadMailbox, myLogin, myPasswd);
+        myMailBox = MailboxFactory::Instance().Create(myUploadMailbox, myLogin, myPasswd);
 	if (myMailBox) {
  		LOG(Log::Info, boost::format( "Logowanie do:  %1%" ) %upload_mailbox);
 // 		LOG(Log::Info, boost::format( "Logowanie do: ...") );
@@ -248,12 +250,13 @@ int CLibMoor::startUpload(unsigned int fromseg) {
 			myUploadFileCRC = myMailBox->getFileCRC();
 			LOG(Log::Debug, boost::format( "CRC Pliku: %1%" ) %myMailBox->getFileCRC());
 
- 			std::cout << generateCleanHashcode() << std::endl;
+                        LOG(Log::Info, generateCleanHashcode());
+
 			for (int i=fromseg; i <= segments; i++) {
 				LOG(Log::Info, boost::format( "Upload segmentu: %1%" )	%i);
                 state = Status::Uploading;
 				ss.str("");
-				
+
 				ss << myUploadFilename << "." << i;
 				if (myMailBox->uploadRequest(ss.str(), address, i) == 0)
 					LOG(Log::Info, boost::format( "Segment %1% wrzucony" )	%i);
@@ -271,27 +274,33 @@ int CLibMoor::startUpload(unsigned int fromseg) {
 }
 
 std::string CLibMoor::generateCleanHashcode() {
-
 	MoorhuntHashEncoder *hashEncoder;
-
-	std::string downpass = "p2mforum.info";
-	std::string editpass = "test";
+hashEncoder = new MoorhuntHashEncoder();
+        std::string downpass = myDownPasswd;
+        std::string editpass = myEditPasswd;
 
 	hashEncoder->setFilename(myUploadFilename);
 	hashEncoder->setCRC(myUploadFileCRC);
 	hashEncoder->setFileSize(myUploadFilesize);
 	hashEncoder->setSegmentCount(myUploadNumOfSeg);
 	hashEncoder->setSegSize(myUploadSegSize);
- 	hashEncoder->setDownloadPassword(downpass);
+        hashEncoder->setDownloadPassword(downpass);
  	hashEncoder->setMirrors(0); // na razie 0 mirrorow
- 	hashEncoder->setEditPassword(editpass);
+        hashEncoder->setEditPassword(editpass);
 
 	std::string hash = hashEncoder->encode();
 
+	delete hashEncoder;
 // 	std::cout << "HASH: " << hash << std::endl;
 	return hash;
 }
 
+std::string CLibMoor::addMirror(std::string editpass, std::string orighash, std::string mboxaddr, std::string mboxpass) {
+ 	MoorhuntHashEncoder *encoder;
+	std::string newhash = encoder->addNewMirror(editpass, orighash, mboxaddr, mboxpass);
+	return newhash;
+// 	delete myhash;
+}
 
 Status CLibMoor::getStatus() {
 	Status s(mySeg, myMailBox->getSpeed(), myMailBox->getBytesRead(),
