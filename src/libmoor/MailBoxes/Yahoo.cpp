bc13e5ebf8bf24385340ee8bb775153e04c72045
mahho <mahho@mahho.net>
2010-03-01 21:10:18 +0100

    licencja lesser gpl, dla lesserow :D

--- src/libmoor/MailBoxes/Yahoo.cpp
+++ src/libmoor/MailBoxes/Yahoo.cpp
@@ -1,15 +1,23 @@
 /*
- * This file is a part of Moorie.
+ * This file is a part of Moorie
  *
- * Copyright (C) 2007 Pawel Stolowski <pawel.stolowski@wp.pl>
+ * Copyright (C) 2007 Moorie Team - http://moorie.pl/
  *
- * Moorie is free software; you can redestribute it and/or modify it
- * under terms of GNU General Public License by Free Software Foundation.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY. See GPL for more details.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-
+               
 #include "Yahoo.h"
 #include "../Log.h"
 #include "../MailboxFactory.h"
